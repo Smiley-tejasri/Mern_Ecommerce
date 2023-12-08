0e17940583d0020190350cf877115bef136a1a83
Tejasri <tejasri@rushelysian.com>
2023-12-08 17:31:20 +0530

    Added Paypal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, createRoutesFromElements,Route,RouterProvider} from 'react-router-dom';
+import {PayPalScriptProvider} from '@paypal/react-paypal-js';
 import { Provider } from 'react-redux';
 import store from './store';
 import './assets/styles/index.css';
@@ -16,6 +17,8 @@ import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const router= createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +33,8 @@ const router= createBrowserRouter(
        <Route  path="/shipping" element={<ShippingScreen/>}/>
        <Route  path="/payment" element={<PaymentScreen/>}/>
        <Route  path="/placeorder" element={<PlaceOrderScreen/>}/>
+       <Route  path="/order/:id" element={<OrderScreen/>}/>
+       <Route  path="/profile" element={<ProfileScreen/>}/>
        </Route>
     </Route>
   )
@@ -39,7 +44,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/>
+      <PayPalScriptProvider deferloading={true}>
+      <RouterProvider router={router}/>
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
